ca8d16ac98a499a6a17e7275cffc1fdf0a2cd89a
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2023-11-21 10:53:28 -0500

    Updated Workout List Component V1

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,14 +1,22 @@
 import React from 'react';
-//Workout List Componenet 
 
+//Workout List Componenet
 const WorkoutList = ({ workouts, onEdit, onDelete }) => {
     return (
       <div>
         <h2>Workout List</h2>
         <ul>
-          {/*//*/}
-        </ul>
-      </div>
-    );
-  };
-  
+        {workouts.map((workout) => (
+          <li key={workout.id}>
+            <strong>{workout.title}</strong>
+            <p>{workout.description}</p>
+            <button onClick={() => onEdit(workout.id)}>Edit</button>
+            <button onClick={() => onDelete(workout.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default WorkoutList;
