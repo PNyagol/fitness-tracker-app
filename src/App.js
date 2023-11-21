18eb14b0775c8d3050593543df40d03f89ccc35b
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2023-11-21 11:53:04 -0500

    Integrated the WorkoutList component to the main App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import WorkoutList from './components/WorkoutList.js';
 
-function App() {
+
+const App = () => {
+  // Sample workout data for demonstration
+  const sampleWorkouts = [
+    { id: 1, title: 'Workout 1', description: 'Description 1' },
+    { id: 2, title: 'Workout 2', description: 'Description 2' },
+    // Add more workout items as needed
+  ];
+
+  // State to manage the workouts
+  const [workouts, setWorkouts] = useState(sampleWorkouts);
+
+  // Placeholder functions for editing and deleting workouts
+  const handleEdit = (id) => {
+    console.log(`Editing workout with id ${id}`);
+  };
+
+  const handleDelete = (id) => {
+    console.log(`Deleting workout with id ${id}`);
+    // Update state to remove the deleted workout
+    setWorkouts(workouts.filter((workout) => workout.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Workout App</h1>
+      {/* Render the WorkoutList component */}
+      <WorkoutList workouts={workouts} onEdit={handleEdit} onDelete={handleDelete} />
+      {/* Additional components or content can be added here */}
     </div>
   );
-}
+};
 
 export default App;
+
