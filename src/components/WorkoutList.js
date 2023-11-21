653b61caff1afa6ff97bfdbbf707a05051c2c349
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2023-11-21 10:41:24 -0500

    WorkoutList Componenet

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,4 +1,14 @@
 import React from 'react';
+//Workout List Componenet 
 
-
-
+const WorkoutList = ({ workouts, onEdit, onDelete }) => {
+    return (
+      <div>
+        <h2>Workout List</h2>
+        <ul>
+          {/*//*/}
+        </ul>
+      </div>
+    );
+  };
+  
