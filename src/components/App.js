f7c83d00a315f84000e1122ae4328d83e3abaae5
Alvine Allan <alvizallaz@gmail.com>
2023-11-23 18:43:39 +0300

    Fixed Navbar.js and App.js errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,184 +1,73 @@
-import React, { useEffect, useState } from "react";
-import NavBar from "./NavBar";
-import WorkoutForm from "./WorkoutForm";
-import WorkoutList from "./WorkoutList";
+
+import React, { useState, useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import WorkoutForm from './WorkoutForm';
+import WorkoutList from './WorkoutList';
 
 function App() {
-  const [workouts, setWorkouts] = useState([]);
-  const [formSubmitted, setFormSubmitted] = useState(false);
-  const [initial] = useState(true);
-  const [isModalOpenned, setIsModalOpenned] = useState(false);
-  const [workoutId, setWorkoutId] = useState(null);
+const [workouts, setWorkouts] = useState([]);
+// const handleWorkoutSubmission = (workoutDetails) => {
+//     setWorkouts((prevWorkouts) => [...prevWorkouts, workoutDetails]);
+//     console.log("Workout details submitted:", workoutDetails);
+//  };
+const handleEdit = (id) => {
+    console.log("Edit workout with id:", id);
+};
 
-  const handleEdit = async (id) => {
-    const patchData = {};
 
-    let data = await fetch(`http://localhost:3001/workouts/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(patchData),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => data)
-      .catch((error) => {
-        console.error("Error during PATCH request:", error);
-      });
-
-    return data;
-  };
-
-  const handleDelete = async (id) => {
-    let data = await fetch(`http://localhost:3001/workouts/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // body:
-    }).then((res) => res.json());
-    console.log("DEMO FROM APP DELETE:", await data);
+  const handleDelete = (id) => {
+    console.log("Delete workout with id:", id);
   };
 
   useEffect(() => {
-    const fetcData = async () => {
-      const response = await fetch("http://localhost:3001/workouts")
-        .then((res) => res.json())
-        .then((data) => setWorkouts(data));
+  const fetchData = async () => {
+    try {
+      let response = await fetch("http://localhost:3001/workouts");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
 
-      return await response;
-    };
+      let data = await response.json();
+      setWorkouts(data);
+      console.log(data); // Log the fetched data, as this will reflect the updated state
+    } catch (error) {
+      console.error("Error fetching workouts:", error);
+    }
+  };
 
-    fetcData();
-  }, []);
+  fetchData();
+}, []); // Empty dependency array to run this effect only once when the component mounts
 
-  useEffect(() => {
-    console.log("workoutId: ", workoutId);
-  }, [workoutId]);
 
   return (
-    <>
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        <WorkoutForm
-          formSubmitted={formSubmitted}
-          setFormSubmitted={setFormSubmitted}
-        />
-        <WorkoutList
-          workouts={workouts}
-          handleDelete={handleDelete}
-          handleEdit={handleEdit}
-          setIsModalOpenned={setIsModalOpenned}
-          isModalOpenned={isModalOpenned}
-          setWorkoutId={setWorkoutId}
-        />
-        {isModalOpenned && (
-          <Modal
-            isModalOpenned={isModalOpenned}
-            setIsModalOpenned={setIsModalOpenned}
-            handleEdit={handleEdit}
-            workoutId={workoutId}
-          />
-        )}
-      </main>
-      <footer>
-        <p>GROUP SEVEN</p>
-      </footer>
-    </>
+    <Router>
+      <div>
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/workout-form" element={<WorkoutForm />} />
+            <Route path="/workout-list" element={<WorkoutList
+            workouts = {workouts}
+            onEdit={handleEdit}
+            onDelete={handleDelete} />} />
+          </Routes>
+        </main>
+        <footer>
+          <p>GROUP SEVEN</p>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
-const Modal = ({
-  setIsModalOpenned,
-  isModalOpenned,
-  handleEdit,
-  workoutId,
-}) => {
-  const [data, setData] = useState(null);
-  useEffect( () => {
-    async function _data() {
-      return await handleEdit(parseInt(workoutId));
-    }
-
-    let data = _data();
-
-    setData(data);
-
-    console.log("DATA: ", data);
-  }, isModalOpenned);
-  return (
-    <div
-      className={"overlay"}
-      onClick={() => setIsModalOpenned(!isModalOpenned)}
-    >
-      <form className="workout-form">
-        <div className="form-group">
-          <label>
-            Exercise:
-            <input
-              type="text"
-              name="exercise"
-              value={data?.exercise}
-              // onChange={handleInputChange}
-              required
-              style={{ marginBottom: "10px" }}
-            />
-          </label>
-        </div>
-
-        <div className="form-group">
-          <label>
-            Duration (minutes):
-            <input
-              type="number"
-              name="duration"
-              value={data?.duration}
-              // onChange={handleInputChange}
-              required
-              style={{ marginBottom: "10px" }}
-            />
-          </label>
-        </div>
-
-        <div className="form-group">
-          <label>
-            Date:
-            <input
-              type="date"
-              name="date"
-              value={data?.date}
-              // onChange={handleInputChange}
-              required
-              style={{ marginBottom: "10px" }}
-            />
-          </label>
-        </div>
-
-        <div className="form-group">
-          <label>
-            Notes:
-            <textarea
-              name="notes"
-              // value={workoutDetails.notes}
-              // onChange={handleInputChange}
-              style={{ marginBottom: "10px" }}
-            />
-          </label>
-        </div>
-
-        <button type="submit" className="small-button">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+// Define a component for the home page
+const Home = () => {
+  return <h1>Welcome to Home!</h1>;
 };
 
 export default App;
+
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#">Home</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-        <Nav.Link href="#workout-form">About Us</Nav.Link>
-          <Nav.Link href="#workout-form">Workout Form</Nav.Link>
-          <Nav.Link href="#workout-list">Workout List</Nav.Link>
-          <Nav.Link href="#fitness-tracker">Fitness Tracker</Nav.Link>
+          <Nav.Link as={Link} to="/workout-form">Workout Form</Nav.Link>
+          <Nav.Link as={Link} to="/workout-list">Workout List</Nav.Link>
+          {/* Add other links here */}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
