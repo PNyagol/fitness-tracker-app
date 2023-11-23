a9f35cd1fc3180080e3d323c7ef1280d82890568
Nyagol <nyagolpeter@gmail.com>
2023-11-23 08:46:55 +0300

    please check chenges for: {NavBar, App.js, WorkoutCard}

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,26 +1,59 @@
+// import React from 'react';
+// import WorkoutItem from './WorkoutItem';
+
+// // Workout List Component
+// const WorkoutList = ({ workouts, onEdit, onDelete }) => {
+
+//   console.log('WORKOULIST:', workouts)
+//   return (
+//     <div>
+//       <h2>Workout List</h2>
+//       <ul>
+//         {/* Added the .reverse method the get the latest object from the DB */}
+//         {workouts?.reverse().map((workout) => (
+//           <WorkoutItem
+//             key={workout.id}
+//             workout={workout}
+//             onEdit={onEdit}
+//             onDelete={onDelete}
+//           />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default WorkoutList;
+
+
+
 import React from 'react';
-import WorkoutItem from './WorkoutItem';
+import WorkoutCard from './WorkoutCard';
 
-// Workout List Component
+
 const WorkoutList = ({ workouts, onEdit, onDelete }) => {
-
-  console.log('WORKOULIST:', workouts)
+  /*
+  can someone make the buttons work now
+  for on edit, they will just reuse the table
+  is the the card implementation is good, we can agree to have the above commented code deleted
+  */ 
   return (
-    <div>
+    <div className="card-container">
       <h2>Workout List</h2>
-      <ul>
-        {/* Added the .reverse method the get the latest object from the DB */}
+      <div className="card-list">
         {workouts?.reverse().map((workout) => (
-          <WorkoutItem
+          <WorkoutCard
             key={workout.id}
-            workout={workout}
-            onEdit={onEdit}
-            onDelete={onDelete}
+            exercise={workout.exercise}
+            duration={workout.duration}
+            date={workout.date}
+            notes={workout.notes}
           />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
 
 export default WorkoutList;
+
